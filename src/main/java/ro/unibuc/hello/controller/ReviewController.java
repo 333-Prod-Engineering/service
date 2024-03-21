3a31dd8bd3973508e3a0d1e64997ed878354de20
Irina Enescu <93870739+irinaenescu2002@users.noreply.github.com>
2024-03-21 08:19:37 +0000

    add update review

--- src/main/java/ro/unibuc/hello/controller/ReviewController.java
+++ src/main/java/ro/unibuc/hello/controller/ReviewController.java
@@ -3,25 +3,38 @@ package ro.unibuc.hello.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import ro.unibuc.hello.data.AuthorEntity;
 import ro.unibuc.hello.data.ReviewEntity;
 import ro.unibuc.hello.dto.ReviewCreationRequestDto;
+import ro.unibuc.hello.dto.ReviewUpdateRequestDto;
 import ro.unibuc.hello.service.ReviewService;
 
 @Controller
-@RequestMapping("/reviews")
 public class ReviewController {
 
     @Autowired
     private ReviewService reviewService;
 
-    @PostMapping
+    @PostMapping("/reviews")
+    @ResponseBody
     public ResponseEntity<ReviewEntity> createReview(@RequestBody ReviewCreationRequestDto reviewDto) {
         ReviewEntity savedReview = reviewService.saveReview(reviewDto);
         return ResponseEntity.ok(savedReview);
+    }
+
+    @PatchMapping("/reviews/{id}")
+    @ResponseBody
+    public ResponseEntity<ReviewEntity> updateAuthor(@PathVariable String id,
+            @RequestBody ReviewUpdateRequestDto updateReviewRequestDto) {
+        var updatedReview = reviewService.updateReview(id, updateReviewRequestDto);
+        return ResponseEntity.ok(updatedReview);
     }
 }
