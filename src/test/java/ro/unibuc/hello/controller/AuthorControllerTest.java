efa823658ffc588f7333cebac856a38e42188bc9
Irina Enescu <93870739+irinaenescu2002@users.noreply.github.com>
2024-03-25 17:34:57 +0000

    add reader controller tests

--- src/test/java/ro/unibuc/hello/controller/AuthorControllerTest.java
+++ src/test/java/ro/unibuc/hello/controller/AuthorControllerTest.java
@@ -1,6 +1,7 @@
 package ro.unibuc.hello.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,101 +32,104 @@ import ro.unibuc.hello.service.AuthorService;
 @ExtendWith(SpringExtension.class)
 public class AuthorControllerTest {
 
-    @Mock
-    private AuthorService authorService;
+        @Mock
+        private AuthorService authorService;
 
-    @InjectMocks
-    private AuthorController authorController;
+        @InjectMocks
+        private AuthorController authorController;
 
-    private MockMvc mockMvc;
+        private MockMvc mockMvc;
 
-    private ObjectMapper objectMapper;
+        private ObjectMapper objectMapper;
 
-    private AuthorEntity author;
+        private AuthorEntity author;
 
-    @BeforeEach
-    public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(authorController).build();
-        objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        author = AuthorEntity.builder().authorId("1").name("Ion Creanga").nationality("romanian")
-                .birthDate(LocalDate.of(1837, 03, 1)).deathDate(LocalDate.of(1889, 12, 31))
-                .build();
-    }
+        @BeforeEach
+        public void setUp() {
+                mockMvc = MockMvcBuilders.standaloneSetup(authorController).build();
+                objectMapper = new ObjectMapper();
+                objectMapper.registerModule(new JavaTimeModule());
+                author = AuthorEntity.builder().authorId("1").name("Ion Creanga").nationality("romanian")
+                                .birthDate(LocalDate.of(1837, 03, 1)).deathDate(LocalDate.of(1889, 12, 31))
+                                .build();
+        }
 
-    @Test
-    void test_GetAuthors() throws Exception {
-        // Given
-        var authorList = List.of(author);
-        when(authorService.getAllAuthors()).thenReturn(authorList);
+        @Test
+        void test_GetAuthors() throws Exception {
+                // Given
+                var authorList = List.of(author);
+                when(authorService.getAllAuthors()).thenReturn(authorList);
 
-        // When
-        var result = mockMvc.perform(get("/authors"))
-                .andExpect(status().isOk())
-                .andReturn();
+                // When
+                var result = mockMvc.perform(get("/authors"))
+                                .andExpect(status().isOk())
+                                .andReturn();
 
-        // Then
-        Assertions.assertEquals(objectMapper.writeValueAsString(authorList),
-                result.getResponse().getContentAsString());
+                // Then
+                Assertions.assertEquals(objectMapper.writeValueAsString(authorList),
+                                result.getResponse().getContentAsString());
 
-    }
+        }
 
-    @Test
-    void test_PostAuthor() throws Exception {
-        // Given
-        var authorCreationRequestDto = AuthorCreationRequestDto.builder().name("Ion Creanga").nationality("romanian")
-                .birthDate(LocalDate.of(1837, 03, 1)).deathDate(LocalDate.of(1889, 12, 31))
-                .build();
-        when(authorService.saveAuthor(any())).thenReturn(author);
+        @Test
+        void test_PostAuthor() throws Exception {
+                // Given
+                var authorCreationRequestDto = AuthorCreationRequestDto.builder().name("Ion Creanga")
+                                .nationality("romanian")
+                                .birthDate(LocalDate.of(1837, 03, 1)).deathDate(LocalDate.of(1889, 12, 31))
+                                .build();
+                when(authorService.saveAuthor(any())).thenReturn(author);
 
-        // When
-        var result = mockMvc.perform(post("/authors")
-                .content(objectMapper.writeValueAsString(authorCreationRequestDto))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn();
+                // When
+                var result = mockMvc.perform(post("/authors")
+                                .content(objectMapper.writeValueAsString(authorCreationRequestDto))
+                                .contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andReturn();
 
-        // Then
-        Assertions.assertEquals(objectMapper.writeValueAsString(author),
-                result.getResponse().getContentAsString());
-    }
+                // Then
+                Assertions.assertEquals(objectMapper.writeValueAsString(author),
+                                result.getResponse().getContentAsString());
+        }
 
-    @Test
-    void test_PatchAuthor() throws Exception {
-        // Given
-        var authorId = "1";
-        var authorUpdateRequestDto = AuthorUpdateRequestDto.builder().deathDate(LocalDate.of(1899, 12, 31)).build();
-        author.setDeathDate(LocalDate.of(1899, 12, 31));
-        when(authorService.updateAuthor(any(), any())).thenReturn(author);
+        @Test
+        void test_PatchAuthor() throws Exception {
+                // Given
+                var authorId = "1";
+                var authorUpdateRequestDto = AuthorUpdateRequestDto.builder().deathDate(LocalDate.of(1899, 12, 31))
+                                .build();
+                author.setDeathDate(LocalDate.of(1899, 12, 31));
+                when(authorService.updateAuthor(any(), any())).thenReturn(author);
 
-        // When
-        var result = mockMvc.perform(patch("/authors/{id}", authorId)
-                .content(objectMapper.writeValueAsString(authorUpdateRequestDto))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn();
+                // When
+                var result = mockMvc.perform(patch("/authors/{id}", authorId)
+                                .content(objectMapper.writeValueAsString(authorUpdateRequestDto))
+                                .contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andReturn();
 
-        // Then
-        Assertions.assertEquals(objectMapper.writeValueAsString(author),
-                result.getResponse().getContentAsString());
-    }
+                // Then
+                Assertions.assertEquals(objectMapper.writeValueAsString(author),
+                                result.getResponse().getContentAsString());
+        }
 
-    @Test
-    void test_DeleteMapping() throws Exception {
-        // Given
-        var authorDeleteRequestDto = AuthorDeleteRequestDto.builder().birthDate(LocalDate.of(1837, 03, 1))
-                .name("Ion Creanga").build();
+        @Test
+        void test_DeleteAuthor() throws Exception {
+                // Given
+                var authorDeleteRequestDto = AuthorDeleteRequestDto.builder().birthDate(LocalDate.of(1837, 03, 1))
+                                .name("Ion Creanga").build();
+                doNothing().when(authorService).deleteAuthor(any());
 
-        // When
-        var result = mockMvc.perform(delete("/authors")
-                .content(objectMapper.writeValueAsString(authorDeleteRequestDto))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn();
+                // When
+                var result = mockMvc.perform(delete("/authors")
+                                .content(objectMapper.writeValueAsString(authorDeleteRequestDto))
+                                .contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andReturn();
 
-        // Then
-        Assertions.assertEquals("Author deleted successfully",
-                result.getResponse().getContentAsString());
-    }
+                // Then
+                Assertions.assertEquals("Author deleted successfully",
+                                result.getResponse().getContentAsString());
+        }
 
 }
