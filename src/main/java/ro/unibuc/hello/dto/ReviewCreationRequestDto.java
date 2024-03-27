ed58cbdfd0dfe77633b480957a1b82923c89e058
Irina Enescu <93870739+irinaenescu2002@users.noreply.github.com>
2024-03-27 22:53:40 +0000

    review controller tests

--- src/main/java/ro/unibuc/hello/dto/ReviewCreationRequestDto.java
+++ src/main/java/ro/unibuc/hello/dto/ReviewCreationRequestDto.java
@@ -1,12 +1,16 @@
 package ro.unibuc.hello.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReviewCreationRequestDto {
     private int rating;
     private String reviewBody;
